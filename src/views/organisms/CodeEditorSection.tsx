d6366a4d9584b2b5129f27aa9c148fcfa6da4d51
DBC-Works <contact.d.b.c@gmail.com>
2024-08-12 16:22:18 +0900

    feat: make headings tab
    close #22

--- src/views/organisms/CodeEditorSection.tsx
+++ src/views/organisms/CodeEditorSection.tsx
@@ -1,30 +1,14 @@
 import { useEffect } from 'react'
-import { useAtom, useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
 
 import { CodeState } from '@/states/types'
-import { evalErrorAtom, runningStateAtom } from '@/states/atoms'
+import { evalErrorAtom } from '@/states/atoms'
+import { useCodeState } from '@/views/hooks/hooks'
 import { CodeEditor } from '@/views/atoms/CodeEditor'
 import { EvalError } from '@/views/atoms/EvalError'
+import { CodeEditorSectionHeader } from './CodeEditorSectionHeader'
 
 type Props = React.ComponentProps<'section'>
-
-/**
- * Use code sate hook
- * @returns Code state
- */
-const useCodeState = (): CodeState => {
-  const { nowPlaying, updated } = useAtomValue(runningStateAtom)
-  const evalError = useAtomValue(evalErrorAtom)
-
-  if (evalError !== null) {
-    return CodeState.Error
-  }
-  if (nowPlaying === false) {
-    return CodeState.Ready
-  }
-
-  return updated !== false ? CodeState.Updated : CodeState.Playing
-}
 
 /**
  * Code editor section
@@ -57,40 +41,21 @@ export const CodeEditorSection: React.FC<Props> = ({
     'grow',
     'mt-1',
   ]
-  let stateText = 'Ready'
-  let stateTextClass = ''
   switch (useCodeState()) {
     case CodeState.Playing:
-      stateText = 'Playing'
-      stateTextClass = 'font-bold text-success'
       classNames.push('textarea-success')
       break
     case CodeState.Updated:
-      stateText = 'Updated'
-      stateTextClass = 'font-bold text-info'
       classNames.push('textarea-info')
       break
     case CodeState.Error:
-      stateText = 'Error'
-      stateTextClass = 'font-bold text-error'
       classNames.push('textarea-error')
       break
   }
 
   return (
     <section className={`${className} flex flex-col`}>
-      <div className="hidden md:flex md:flex-row">
-        <h2 className="flex-auto text-base font-bold">
-          <label htmlFor="code">Code to run</label>
-        </h2>
-        <div
-          role="status"
-          aria-live="polite"
-          className={`px-1 ${stateTextClass}`}
-        >
-          {stateText}
-        </div>
-      </div>
+      <CodeEditorSectionHeader controls="id" />
       <div className={classNames.join(' ')}>
         <CodeEditor id="code" />
       </div>
